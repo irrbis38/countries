725c48367983aa61b04da1bd66fd09f815bf80e1
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-09-13 13:54:52 +0300

    feat: implement toggle theme

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ const store = createStore(
   rootReducer,
   composeEnhancers(
     applyMiddleware(
-      thunk.withExtraArguments({
+      thunk.withExtraArgument({
         client: axios,
         api,
       })
