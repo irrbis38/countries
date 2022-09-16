7c33da30bd64bf60b0f53de59aaa9ca2002726c8
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-09-16 12:44:34 +0300

    feat: add show border countries

--- src/store/details/details-actions.js
+++ src/store/details/details-actions.js
@@ -1,6 +1,8 @@
 export const SET_LOADING = "@@details/SET_LOADING";
 export const SET_ERROR = "@@details/SET_ERROR";
 export const SET_COUNTRY = "@@details/SET_COUNTRY";
+export const CLEAR_DETAILS = "@@details/CLEAR_DETAILS";
+export const SET_NEIGHBORS = "@@details/SET_NEIGHBORS";
 
 const setLoading = () => ({
   type: SET_LOADING,
@@ -13,6 +15,14 @@ const setCountry = (country) => ({
   type: SET_COUNTRY,
   payload: country,
 });
+const setNeighbors = (countries) => ({
+  type: SET_NEIGHBORS,
+  payload: countries,
+});
+
+export const clearDetails = () => ({
+  type: CLEAR_DETAILS,
+});
 
 export const loadCountryByName =
   (name) =>
@@ -23,4 +33,13 @@ export const loadCountryByName =
       .get(api.searchByCountry(name))
       .then(({ data }) => dispatch(setCountry(data[0])))
       .catch((err) => dispatch(setError(err.message)));
+  };
+
+export const loadNeighborsByBorder =
+  (borders) =>
+  (dispatch, _, { client, api }) => {
+    client
+      .get(api.filterByCode(borders))
+      .then(({ data }) => dispatch(setNeighbors(data.map((c) => c.name))))
+      .catch(console.error);
   };
